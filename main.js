3c65ccc9abd133674caf255cf26e187778a5d5bc
Mica <110005136+0mimica0@users.noreply.github.com>
2022-08-28 12:52:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+alert("victor puto");
+
+var nombre = "Mica";
+
+var altura = 150;
+
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById("datos");
+datos.innerHTML = `
+<h1>no se, victor puto</h1>
+<h2>la creadora de esta para nada improvisada poronga es: ${nombre}</h2>
+<h3>Mido: ${altura} cm</h3>
+<h4>no te rias, ya soy que soy chiquita, lidio con eso todos los dias</h4>
+<h5>hijodeputa</h5>
+`;
+
